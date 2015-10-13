94c8e8fabfef3da5a35450dff57c1a2a8071a660
Murali Krishna Valluri <mvallu2@uic.edu>
2015-10-12 19:55:51 -0500

    Added parser and data file needed for the project.

---
+++ parser/DataCleanupProj/DataCleanupProj/Properties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCleanupProj
+{
+    class Properties
+    {
+        public string id { get; set; }
+
+        public string basin { get; set; }
+
+        public string name { get; set; }
+
+        public TimeStamp timestamp { get; set; }
+
+        public float maxwind { get; set; }
+
+        public float minpressure { get; set; }
+
+        public string latitude { get; set; }
+
+        public string longitude { get; set; }
+
+        public string l { get; set; }
+
+        public string ts { get; set; }
+
+        public Quarter q34 { get; set; }
+
+        public Quarter q50 { get; set; }
+
+        public Quarter q64 { get; set; }
+
+        public Properties()
+        {
+            timestamp = new TimeStamp();
+            q34 = new Quarter();
+            q50 = new Quarter();
+            q64 = new Quarter();
+        }
+    }
+}
---
+++ parser/DataCleanupProj/DataCleanupProj/TimeStamp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCleanupProj
+{
+    class TimeStamp
+    {
+        public int year { get; set; }
+
+        public int month { get; set; }
+
+        public int day { get; set; }
+
+        public int hour { get; set; }
+
+        public int minute { get; set; }
+    }
+
+}
