fa348bbb761c46b844f2704e997a34dc62a9ff23
Murali Krishna Valluri <mvallu2@uic.edu>
2015-10-14 19:54:08 -0500

    Filtered data
    Added code related to filtered data and also added the output file

---
+++ parser/DataCleanupProj/FilteredData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCleanupProj
+{
+    class FilteredData
+    {
+        public string name { get; set; }
+
+        public string id { get; set; }
+
+        public int year { get; set; }
+
+        public Wind wind { get; set; }
+
+        public Pressure pressure { get; set; }
+
+        public FilteredData()
+        {
+            wind = new Wind();
+
+            pressure = new Pressure();
+        }
+    }
+
+    class Wind
+    {
+        public float min { get; set; }
+
+        public float max { get; set; }
+
+        public float avg { get; set; }
+    }
+
+    class Pressure
+    {
+        public float min { get; set; }
+
+        public float max { get; set; }
+
+        public float avg { get; set; }
+    }
+}
--- parser/DataCleanupProj/Program.cs
+++ parser/DataCleanupProj/Program.cs
@@ -10,9 +10,11 @@ namespace DataCleanupProj
     {
         static string finalData = "";
         static DataWrapper outputData;
+        static List<FilteredData> filteredData;
         static void Main(string[] args)
         {
             outputData = new DataWrapper();
+            filteredData = new List<FilteredData>();
 
             var data = File.ReadAllLines(@"D:\Projects\hurdat2-1851-2014-060415.txt");
             var data2 = File.ReadAllLines(@"D:\Projects\hurdat2-nencpac-1949-2014-092515.txt");
@@ -22,6 +24,8 @@ namespace DataCleanupProj
             settings.NullValueHandling = NullValueHandling.Ignore;
             finalData = JsonConvert.SerializeObject(outputData, settings);
             File.WriteAllText(@"D:\Projects\data.json", finalData);
+            finalData = JsonConvert.SerializeObject(filteredData, settings);
+            File.WriteAllText(@"D:\Projects\filteredData.json", finalData);
             Console.ReadLine();
         }
 
@@ -31,6 +35,19 @@ namespace DataCleanupProj
             string name = "";
             string basin = "";
             string id = "";
+            int year = 0;
+            FilteredData tempFilteredData;
+
+            float wMin = 99999999.0f;
+            float wMax = 0.0f;
+            float wAvg = 0.0f;
+
+            float pMin = 99999999.0f;
+            float pMax = 0.0f;
+            float pAvg = 0.0f;
+
+            int count = 0;
+
             foreach (var line in data)
             {
                 Feature tempFeature = new Feature();
@@ -50,24 +67,60 @@ namespace DataCleanupProj
                     string l = tempLines[5].Substring(0, tempLines[5].Length - 1).Trim();
                     string k = tempLines[4].Substring(0, tempLines[4].Length - 1).Trim();
                     tempFeature.geometry.coordinates = new Decimal[] { Convert.ToDecimal(l) * -1, Convert.ToDecimal(k) };
+
                     tempFeature.properties.maxwind = Convert.ToInt64(tempLines[6].Trim());
+                    if (wMax < tempFeature.properties.maxwind)
+                        wMax = tempFeature.properties.maxwind;
+                    if (wMin > tempFeature.properties.maxwind)
+                        wMin = tempFeature.properties.maxwind;
+                    wAvg += tempFeature.properties.maxwind;
+
                     tempFeature.properties.minpressure = Convert.ToInt64(tempLines[7].Trim());
+                    if (pMax < tempFeature.properties.minpressure)
+                        pMax = tempFeature.properties.minpressure;
+                    if (pMin > tempFeature.properties.minpressure)
+                        pMin = tempFeature.properties.minpressure;
+                    pAvg += tempFeature.properties.minpressure;
+
+
                     tempFeature.properties.q34 = GetQuarter(tempLines[8].Trim(), tempLines[9].Trim(), tempLines[10].Trim(), tempLines[11].Trim());
                     tempFeature.properties.q50 = GetQuarter(tempLines[12].Trim(), tempLines[13].Trim(), tempLines[14].Trim(), tempLines[15].Trim());
                     tempFeature.properties.q64 = GetQuarter(tempLines[16].Trim(), tempLines[17].Trim(), tempLines[18].Trim(), tempLines[19].Trim());
                     features.Add(tempFeature);
+                    count++;
                 }
                 else
                 {
+                    if(count > 0)
+                    {
+                        wAvg /= count;
+                        pAvg /= count;
+                        tempFilteredData = new FilteredData();
+                        tempFilteredData.name = name;
+                        tempFilteredData.id = id;
+                        tempFilteredData.year = year;
+                        tempFilteredData.wind.avg = wAvg;
+                        tempFilteredData.wind.max = wMax;
+                        tempFilteredData.wind.min = wMin;
+                        tempFilteredData.pressure.avg = pAvg;
+                        tempFilteredData.pressure.min = pMin;
+                        tempFilteredData.pressure.max = pMax;
+                        filteredData.Add(tempFilteredData);
+                        count = 0;
+                        pMin = wMin = 9999999.0f;
+                        pMax = wMax = 0.0f;
+                        pAvg = wAvg = 0.0f;
+                    }
                     name = tempLines[1].Trim();
                     id = tempLines[0].Trim();
                     basin = tempLines[0].Trim().Substring(0, 2);
+                    year = Convert.ToInt32(tempLines[0].Trim().Substring(4, 4));
                 }
             }
             //finalData += JsonConvert.SerializeObject(features);
             return features;
         }
-
+        
         static TimeStamp GetTimeStamp(string line)
         {
             TimeStamp t = new TimeStamp();
